529d4d4ea7a078a476fb033c3dbba3b8beb8b334
alex <alex@example.com>
2021-02-22 22:19:37 +0800

    u

--- clear.bat
+++
@@ -1,4 +0,0 @@
-rmdir /S /Q src\main\resources\static\fontawesome\less
-rmdir /S /Q src\main\resources\static\fontawesome\metadata
-rmdir /S /Q src\main\resources\static\fontawesome\scss
-del /S /Q src\main\resources\static\bootstrap\*.map
